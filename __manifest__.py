c846755dedcd4cbc29c4bd95ee26286cf2483a6c
jwaes <jaco.waes@gmail.com>
2022-04-21 10:49:11 +0000

    changed order of imports

--- __manifest__.py
+++ __manifest__.py
@@ -22,8 +22,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/report_layout.xml',
         'views/report_templates.xml',
+        'views/report_layout.xml',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
