39c660892431e214188e94a839636f22afa77741
jwaes <jaco.waes@gmail.com>
2022-04-21 11:03:31 +0000

    moved to data

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '15.1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','web'],
@@ -23,7 +23,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/report_templates.xml',
-        'views/report_layout.xml',
+        'data/report_layout.xml',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
