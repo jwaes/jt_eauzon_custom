759397ec8135709a91956dc535e9f55815768862
jwaes <jaco.waes@gmail.com>
2023-09-07 14:06:48 +0000

    hs codes

--- __manifest__.py
+++ __manifest__.py
@@ -14,10 +14,10 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.11',
+    'version': '0.12',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','web','purchase','stock','sale_management'],
+    'depends': ['base','web','purchase','stock','sale_management','account'],
 
     # always loaded
     'data': [
@@ -25,6 +25,7 @@
         'report/product_product_templates.xml',
         'report/purchase_order_templates.xml',
         'report/purchase_quotation_templates.xml',
+        'report/report_invoice.xml',
         'views/report_templates.xml',
         'views/sale_views.xml',
         'data/report_layout.xml',
