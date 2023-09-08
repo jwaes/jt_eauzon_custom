900e93a208d97bca355481615bc516537217257c
jwaes <jaco.waes@gmail.com>
2023-09-08 08:46:56 +0000

    rounding

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.16',
+    'version': '0.17',
 
     # any module necessary for this one to work correctly
     'depends': ['base','web','purchase','stock','sale_management','account'],
@@ -41,5 +41,5 @@
         'web.report_assets_common': [
             'jt_eauzon_custom/static/src/scss/layout_eauzon.scss',
         ],
-    }    
+    }
 }
