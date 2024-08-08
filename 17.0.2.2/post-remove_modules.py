c48c425919e28edfa6c3bf984d6e5cd30675e332
jwaes <jaco.waes@gmail.com>
2024-08-08 12:40:52 +0000

    pre

--- 17.0.2.2/post-remove_modules.py
+++ 17.0.2.2/post-remove_modules.py
@@ -18,4 +18,4 @@ def migrate(cr, version):
 
     # util.remove_theme(cr, 'theme_now')
 
-    # util.update_record_from_xml(cr, 'sale.mail_template_sale_confirmation' )
+    util.update_record_from_xml(cr, 'jt_stock_portal.transfer_portal_content' )
