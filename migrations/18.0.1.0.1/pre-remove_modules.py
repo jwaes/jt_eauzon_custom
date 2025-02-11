05162b8232c26e0dcc1ea35b0640b1f5f91bab73
jwaes <jaco.waes@gmail.com>
2025-02-11 14:13:03 +0000

    remove old modules

--- migrations/18.0.1.0.1/pre-remove_modules.py
+++ migrations/18.0.1.0.1/pre-remove_modules.py
@@ -6,5 +6,20 @@ _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
+
+    
+    modules_to_uninstall = [
+        'jt_account_external',
+        'jt_account_nobanner',
+        'jt_account_sepa',
+        'jt_debrand',
+        'jt_hr_workday',
+        'jt_invoice_cashdiscount',
+        'jt_sale_order_line_codecolumn',        
+    ]
+
+    for candidate in modules_to_uninstall:
+        _logger.info("About to uninstall module %s", candidate)
+        util.uninstall_module(cr,candidate)    
     
     util.remove_view(cr, xml_id='jt_eauzon_custom.report_purchaseorder_document')
